16edb4e9c915841fc5d0a3146061e8a9fa30804f
Jerker Bäckström <jerker.backstrom@outlook.com>
2020-01-30 23:21:31 +0200

    Add departure and arrival station selects

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,25 @@ import styled from 'styled-components';
 import DayPicker from './DayPicker';
 import ConnectionList from './ConnectionList';
 import fetchConnections from './fetch-connections';
+import stations from './stations';
 
 const ConnectionsWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
 
+export const StyledSelect = styled.select`
+  height: 40px;
+  padding: 5px;
+  font-size: 16px;
+  border: 2px solid grey;
+  border-radius: 5px;
+`;
+
+
 function App() {
-  const [departure, setDeparture] = useState('TKU');
-  const [arrival, setArrival] = useState('VMO');
+  const [departure, setDeparture] = useState('');
+  const [arrival, setArrival] = useState('');
   const [dateTime, setDateTime] = useState(moment().format('YYYY-MM-DD'));
   const [connections, setConnections] = useState({
     departure: [],
@@ -21,16 +31,26 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      setConnections({
-        departure: await fetchConnections(departure, arrival, dateTime),
-        arrival: await fetchConnections(arrival, departure, dateTime)
-      });
+      if (departure && arrival) {
+        setConnections({
+          departure: await fetchConnections(departure, arrival, dateTime),
+          arrival: await fetchConnections(arrival, departure, dateTime)
+        });
+      }
     })();
   }, [departure, arrival, dateTime]);
 
   return (
     <div>
       <h1>Junien hinnat</h1>
+      <StyledSelect onChange={e => setDeparture(e.target.value)} value={departure}>
+        <option value="" disabled hidden>Mistä</option>
+        {stations.map(station => <option key={station.stationShortCode} value={station.stationShortCode}>{station.stationName}</option>)}
+      </StyledSelect>
+      <StyledSelect onChange={e => setArrival(e.target.value)} value={arrival}>
+        <option value="" disabled hidden>Minne</option>
+        {stations.map(station => <option key={station.stationShortCode} value={station.stationShortCode}>{station.stationName}</option>)}
+      </StyledSelect>
       <DayPicker value={dateTime} onDayChange={setDateTime} />
       <ConnectionsWrapper>
         <ConnectionList departure={departure} arrival={arrival} connections={connections.departure} />
