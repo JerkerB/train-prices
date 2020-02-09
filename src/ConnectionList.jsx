1fe19ca6bf7fb657f03c2e8277290a1278f9a417
Jerker Bäckström <jerker.backstrom@outlook.com>
2020-02-09 12:09:06 +0200

    Show spinner when fetching connections

--- src/ConnectionList.jsx
+++ src/ConnectionList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import stations from './stations';
 import fetchConnections from './fetch-connections';
+import Spinner from './Spinner';
 
 function stationName(stationShortCode) {
   const station = stations.find(station => station.stationShortCode === stationShortCode);
@@ -13,22 +14,30 @@ const Wrapper = styled.div`
 `;
 
 function ConnectionList({ departure, arrival, dateTime }) {
-  const [connections, setConnections] = useState([]);
+  const [connections, setConnections] = useState();
 
   useEffect(() => {
     (async () => {
+      setConnections();
       setConnections(await fetchConnections(departure, arrival, dateTime));
     })();
   }, [departure, arrival, dateTime]);
 
-  return (
-    <Wrapper>
-      <h2>{`${stationName(departure)} - ${stationName(arrival)}`}</h2>
-      {connections.map(conn => (
+  let connectionList = <Spinner />;
+
+  if (connections) {
+    connectionList =
+      connections.map(conn => (
         <div key={conn.id}>
           <h3>{`${conn.departureTime} - ${conn.arrivalTime} ${conn.price}`}</h3>
         </div>
-      ))}
+      ));
+  }
+
+  return (
+    <Wrapper>
+      <h2>{`${stationName(departure)} - ${stationName(arrival)}`}</h2>
+      {connectionList}
       <a target="_blank" href={`https://uusi.vr.fi/kertalippu-menomatkan-hakutulokset?from=${departure}&to=${arrival}&outbound=${dateTime}`}>
         Siirry varaamaan liput
       </a>
